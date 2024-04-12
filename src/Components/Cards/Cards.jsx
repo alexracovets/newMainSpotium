de57de42ec351ca15a3c4d47310a8522a0251d61
Oleksand Rakovets <57405165+alexracovets@users.noreply.github.com>
2024-04-12 11:01:49 +0300

    test

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,15 +1,40 @@
 import Card from './Card/Card';
-
+import { FlapDisplay, Presets } from "react-split-flap-effect";
 import s from './Cards.module.scss';
+import { useEffect, useState } from 'react';
 
 const MAX_LENGTH = 11;
 const initialArray = new Array(MAX_LENGTH).fill('');
 
+// export default function Cards() {
+//     return (
+//         <span className={s.cards}>
+//             {initialArray.map((_, i) => <Card key={i} index={i} />)}
+//             {initialArray.map((_, i) => <Card key={i + MAX_LENGTH} index={i + MAX_LENGTH} />)}
+//         </span>
+//     )
+// }
+const sentences = [
+    'software   development',
+    'asdasd asdas asdasdsa',
+];
 export default function Cards() {
+    const [activeSentences, setActiveSentences] = useState(0);
+    const animation = () => {
+        setInterval(() => {
+            setActiveSentences((prev) => (prev + 1) % sentences.length);
+        }, 5000);
+    }
+    useEffect(() => {
+        animation();
+    }, []);
+
     return (
-        <span className={s.cards}>
-            {initialArray.map((_, i) => <Card key={i} index={i} />)}
-            {initialArray.map((_, i) => <Card key={i + MAX_LENGTH} index={i + MAX_LENGTH} />)}
-        </span>
+        <FlapDisplay
+            className='flip'
+            chars={Presets.ALPHANUM + ",!"}
+            length={22}
+            value={sentences[activeSentences]}
+        />
     )
 } 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import React from 'react'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
